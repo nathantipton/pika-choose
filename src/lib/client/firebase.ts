313d7559fe44b0176c39893f54a89e14646afeb2
nathantipton <ntipton21@gmail.com>
2024-02-21 19:45:54 -0500

    analytics

--- src/lib/client/firebase.ts
+++ src/lib/client/firebase.ts
@@ -3,6 +3,7 @@ import { derived, writable, type Readable } from "svelte/store";
 
 import { invalidateAll } from "$app/navigation";
 import { AuthStatus } from "$lib/models/auth";
+import { getAnalytics, initializeAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import type { User } from "firebase/auth";
 import { getAuth, onIdTokenChanged } from "firebase/auth";
@@ -18,6 +19,7 @@ export const auth = getAuth();
 export const storage = getStorage();
 export const authStatus = writable<AuthStatus>(AuthStatus.NotInitialized);
 export const authUpdated = derived(authStatus, ($authStatus) => $authStatus !== AuthStatus.NotInitialized);
+export const analytics = getAnalytics(app);
 
 export const magicLinkActionCodeSettings = {
     url: `${PUBLIC_DOMAIN}/sign-in/magic-link`,
--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,8 +1,5 @@
 <script>
-	import { goto } from '$app/navigation';
-	import { userData } from '$lib/client/firebase';
 	import AuthCheck from '$lib/components/ui/auth/AuthCheck.svelte';
-	import { onMount } from 'svelte';
 </script>
 
 <AuthCheck>
